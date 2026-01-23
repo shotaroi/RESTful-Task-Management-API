f3cb36eb921ecee2c8e5d1beb6bc3223aeb20dd9
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-23 18:06:26 +0100

    Create ResTfulTaskManagementApiApplicationTests.java

---
+++ src/test/java/com/shotaroi/restfultaskmanagementapi/ResTfulTaskManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shotaroi.restfultaskmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulTaskManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
