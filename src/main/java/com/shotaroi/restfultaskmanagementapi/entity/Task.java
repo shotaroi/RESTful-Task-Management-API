afd6670f50d77c630b91f1d841fc5e3e2abe2127
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-24 00:30:59 +0100

    Update Task.java

--- src/main/java/com/shotaroi/restfultaskmanagementapi/entity/Task.java
+++ src/main/java/com/shotaroi/restfultaskmanagementapi/entity/Task.java
@@ -1,4 +1,47 @@
 package com.shotaroi.restfultaskmanagementapi.entity;
 
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tasks")
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private boolean done = false;
+
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @PrePersist
+    void onCreate() {
+        this.createdAt = Instant.now();
+    }
+
+    public Task() {}
+
+    public Task(String title) {
+        this.title = title;
+        this.done = false;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) {this.title = title; }
+
+    public boolean isDone() { return done; }
+    public void setDone(boolean done) { this.done = done; }
+
+    public Instant getCreatedAt() { return createdAt; }
+    public void setCreatedAt(Instant createdAt) { this.createdAt = createdAt; }
 }
