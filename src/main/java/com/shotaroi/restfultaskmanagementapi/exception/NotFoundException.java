8be812e14189d47c04f28dbb70eabbef2509a243
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-24 00:30:54 +0100

    Create NotFoundException.java

---
+++ src/main/java/com/shotaroi/restfultaskmanagementapi/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.shotaroi.restfultaskmanagementapi.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
