7d5f7d556484c2a331eafdc2a8840cc59791647b
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-25 00:31:50 +0100

    Update TaskRepository.java

--- src/main/java/com/shotaroi/restfultaskmanagementapi/repository/TaskRepository.java
+++ src/main/java/com/shotaroi/restfultaskmanagementapi/repository/TaskRepository.java
@@ -1,7 +1,11 @@
 package com.shotaroi.restfultaskmanagementapi.repository;
 
+import com.shotaroi.restfultaskmanagementapi.entity.AppUser;
 import com.shotaroi.restfultaskmanagementapi.entity.Task;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    Page<Task> findByOwner(AppUser owner, Pageable pageable);
 }
