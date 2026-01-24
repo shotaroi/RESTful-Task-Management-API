a929fb8a8b2723033e04af8e5d066e6a6225374d
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-25 00:31:46 +0100

    Create CustomUserDetailsService.java

---
+++ src/main/java/com/shotaroi/restfultaskmanagementapi/security/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.shotaroi.restfultaskmanagementapi.security;
+
+import com.shotaroi.restfultaskmanagementapi.entity.AppUser;
+import com.shotaroi.restfultaskmanagementapi.repository.UserRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return User.withUsername(user.getUsername())
+                .password((user.getPasswordHash()))
+                .roles("USER")
+                .build();
+    }
+}
