45329f36820fe950f09cf39e22d3c5c11db5c59c
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-25 00:31:57 +0100

    Create AppUser.java

---
+++ src/main/java/com/shotaroi/restfultaskmanagementapi/entity/AppUser.java
@@ -0,0 +1,32 @@
+package com.shotaroi.restfultaskmanagementapi.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    public AppUser() {}
+
+    public AppUser(String username, String passwordHash) {
+        this.username = username;
+        this.passwordHash = passwordHash;
+    }
+
+    public Long getId() { return id; }
+
+    public String getUsername() { return username; }
+    public void setUsername() { this.username = username; }
+
+    public String getPasswordHash() { return passwordHash; }
+    public void setPasswordHash(String passwordHash) { this.passwordHash = passwordHash; }
+}
