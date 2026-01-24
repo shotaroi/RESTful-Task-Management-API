3c1320184e46257e0aaeb81a80ad205752c7fa28
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-24 21:22:50 +0100

    Update TaskRepository.java

--- src/main/java/com/shotaroi/restfultaskmanagementapi/repository/TaskRepository.java
+++ src/main/java/com/shotaroi/restfultaskmanagementapi/repository/TaskRepository.java
@@ -1,5 +1,6 @@
 package com.shotaroi.restfultaskmanagementapi.repository;
 
+import com.shotaroi.restfultaskmanagementapi.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
