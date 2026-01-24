fb607d2ae6a06d0e5fd6d9bbe718ca3b90068326
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-25 00:32:03 +0100

    Create AuthResponse.java

---
+++ src/main/java/com/shotaroi/restfultaskmanagementapi/dto/AuthResponse.java
@@ -0,0 +1,12 @@
+package com.shotaroi.restfultaskmanagementapi.dto;
+
+import org.springframework.security.authorization.MapRequiredAuthoritiesRepository;
+
+public class AuthResponse {
+    private String token;
+
+    public AuthResponse() {}
+    public AuthResponse(String token) { this.token = token; }
+
+    public String getToken() { return token; }
+}
