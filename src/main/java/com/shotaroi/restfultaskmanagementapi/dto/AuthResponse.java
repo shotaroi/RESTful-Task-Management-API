c8fca1b9548cd29b45ddc56831eb581cefd69cdf
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-28 00:34:59 +0100

    Update AuthResponse.java

--- src/main/java/com/shotaroi/restfultaskmanagementapi/dto/AuthResponse.java
+++ src/main/java/com/shotaroi/restfultaskmanagementapi/dto/AuthResponse.java
@@ -9,4 +9,7 @@ public class AuthResponse {
     public AuthResponse(String token) { this.token = token; }
 
     public String getToken() { return token; }
+
+    public void setToken(String token) { this.token = token; }
+
 }
