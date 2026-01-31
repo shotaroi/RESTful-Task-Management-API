36ff808b15d44535377c7e97aedd1474b0c8be51
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-01-31 19:03:54 +0100

    Update AppUser.java

--- src/main/java/com/shotaroi/restfultaskmanagementapi/entity/AppUser.java
+++ src/main/java/com/shotaroi/restfultaskmanagementapi/entity/AppUser.java
@@ -25,7 +25,7 @@ public class AppUser {
     public Long getId() { return id; }
 
     public String getUsername() { return username; }
-    public void setUsername() { this.username = username; }
+    public void setUsername(String username) { this.username = username; }
 
     public String getPasswordHash() { return passwordHash; }
     public void setPasswordHash(String passwordHash) { this.passwordHash = passwordHash; }
